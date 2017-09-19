0f213293f4e12ce6bb82766a03a97f7d9498b0a4
vondacho <olivier.vondach@obya.ch>
2017-09-19 14:35:15 +0200

    downgraded primeng

--- myoffice-customer-ui/frontend/src/app/customer/component/primeng/customer-components.module.ts
+++ myoffice-customer-ui/frontend/src/app/customer/component/primeng/customer-components.module.ts
@@ -1,23 +1,28 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {CalendarModule} from 'primeng/primeng';
 import {CustomerEditComponent} from './customer-edit/customer-edit.component';
 import {FolderEditComponent} from './folder-edit/folder-edit.component';
-import { FolderListComponent } from './folder-list/folder-list.component';
-import { CustomerListComponent } from './customer-list/customer-list.component';
+import {FolderListComponent} from './folder-list/folder-list.component';
+import {CustomerListComponent} from './customer-list/customer-list.component';
+import {CustomerSearchComponent} from './customer-search/customer-search.component';
+import {FolderSearchComponent} from './folder-search/folder-search.component';
 
 @NgModule({
-  imports: [
-    CalendarModule
-  ],
-  declarations: [
-    CustomerEditComponent,
-    FolderEditComponent,
-    FolderListComponent,
-    CustomerListComponent,
-  ],
-  exports: [
-    CustomerEditComponent,
-    FolderEditComponent,
-  ]
+    imports: [
+        CalendarModule
+    ],
+    declarations: [
+        CustomerEditComponent,
+        CustomerListComponent,
+        CustomerSearchComponent,
+        FolderEditComponent,
+        FolderListComponent,
+        FolderSearchComponent
+    ],
+    exports: [
+        CustomerEditComponent,
+        FolderEditComponent,
+    ]
 })
-export class CustomerComponentsModule { }
+export class CustomerComponentsModule {
+}
