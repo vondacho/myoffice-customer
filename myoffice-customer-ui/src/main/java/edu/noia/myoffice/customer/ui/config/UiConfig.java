de295bfe9d67646f7ff3e06b611ee1f0dacaf5be
vondacho <olivier.vondach@obya.ch>
2017-09-23 19:33:17 +0200

    added configuration class for static resources serving

---
+++ myoffice-customer-ui/src/main/java/edu/noia/myoffice/customer/ui/config/UiConfig.java
@@ -0,0 +1,18 @@
+package edu.noia.myoffice.customer.ui.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class UiConfig extends WebMvcConfigurerAdapter {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+        "classpath:/static/"
+    };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+}
--- myoffice-customer-ui/src/main/java/edu/noia/myoffice/customer/ui/rest/UiResource.java
+++ myoffice-customer-ui/src/main/java/edu/noia/myoffice/customer/ui/rest/UiResource.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 import static edu.noia.myoffice.customer.ui.rest.UiResource.UI_ENDPOINT_PATH;
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -16,7 +18,9 @@ public class UiResource {
     public static final String UI_ENDPOINT_PATH = "/hello";
 
     @GetMapping
-    public ResponseEntity create(@RequestParam(name = "who") String who) {
-        return ok("Hello " + who);
+    public ResponseEntity create(@RequestParam(name = "who", required = false) String who) {
+        return ok(Optional.ofNullable(who)
+                .map(w -> "Hello " + w + "!")
+                .orElse("Hello world!"));
     }
 }
