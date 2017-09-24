4dce30256515601eb51cc13d48b74965f79042f9
vondacho <olivier.vondach@obya.ch>
2017-09-24 12:27:21 +0200

    removed hash routing, added karma istambul, added tests when build client

--- myoffice-customer-ui/frontend/karma.conf.js
+++ myoffice-customer-ui/frontend/karma.conf.js
@@ -1,33 +1,69 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const path = require('path');
+
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
-    ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
-    },
-    angularCli: {
-      environment: 'dev'
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
-  });
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine', '@angular/cli'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-phantomjs-launcher'),
+            require('karma-chrome-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage-istanbul-reporter'),
+            require('karma-istanbul-threshold'),
+            require('karma-junit-reporter'),
+            require('@angular/cli/plugins/karma')
+        ],
+        client: {
+            clearContext: false // leave Jasmine Spec Runner output visible in browser
+        },
+        junitReporter: {
+            outputDir: path.join(__dirname, '/coverage/junit'), // results will be saved as $outputDir/$browserName.xml
+            useBrowserName: true, // add browser name to report and classes names
+            properties: {}, // key value pair of properties to add to the <properties> section of the report
+            xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+        },
+        coverageIstanbulReporter: {
+            reports: ['html', 'lcovonly', 'json'],
+            dir: path.join(__dirname, '/coverage'),
+            fixWebpackSourcePaths: true,
+            skipFilesWithNoCoverage: true,
+            'report-config': {
+                html: {
+                    subdir: 'html'
+                }
+            }
+        },
+        istanbulThresholdReporter: {
+            src: 'coverage/coverage-final.json',
+            reporters: ['text'],
+            thresholds: {
+                global: {
+                    statements: 90,
+                    branches: 90,
+                    lines: 70,
+                    functions: 90,
+                },
+                each: {
+                    statements: 80,
+                    branches: 80,
+                    lines: 60,
+                    functions: 80,
+                },
+            }
+        },
+        angularCli: {
+            environment: 'dev'
+        },
+        reporters: config.angularCli && config.angularCli.codeCoverage ? ['progress', 'kjhtml', 'junit', 'coverage-istanbul', 'istanbul-threshold'] : ['progress', 'kjhtml', 'junit'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['Chrome'],
+        singleRun: false
+    });
 };
--- myoffice-customer-ui/src/main/java/edu/noia/myoffice/customer/ui/config/ClientSidePathLocationStrategySupportConfiguration.java
+++ myoffice-customer-ui/src/main/java/edu/noia/myoffice/customer/ui/config/ClientSidePathLocationStrategySupportConfiguration.java
@@ -1,5 +1,6 @@
 package edu.noia.myoffice.customer.ui.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.web.ErrorViewResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,18 +9,20 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Collections;
 
-//@Configuration
+@Slf4j
+@Configuration
 public class ClientSidePathLocationStrategySupportConfiguration {
 
-  /*
-   * Collaboration behaviour for Angular PathLocationStrategy
-   * https://codecraft.tv/courses/angular/routing/routing-strategies/
-   */
-  @Bean
-  ErrorViewResolver supportPathLocationStrategy() {
-    return (request, status, model) -> status == HttpStatus.NOT_FOUND
-      ? new ModelAndView("index.html", Collections.emptyMap(), HttpStatus.OK)
-      : null;
-  }
+    /*
+     * Collaboration behaviour for Angular PathLocationStrategy
+     * https://codecraft.tv/courses/angular/routing/routing-strategies/
+     */
+    @Bean
+    ErrorViewResolver supportPathLocationStrategy() {
+        LOG.info("supporting 404");
+        return (request, status, model) -> status == HttpStatus.NOT_FOUND
+                ? new ModelAndView("index.html", Collections.emptyMap(), HttpStatus.OK)
+                : null;
+    }
 
 }
